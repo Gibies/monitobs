60095c687906b3245cb63271368669e2b13df7ee
gayathrijayank <gayathrijayank@gmail.com>
2023-05-10 11:44:25 +0530

    Updated monitobs for

--- pylib/daview.py
+++ pylib/daview.py
@@ -6,36 +6,25 @@ Created on Thu Jun 25 12:28:25 2020
 @author: gibies
 """
 
-import sys,os
-CURR_PATH=os.path.dirname(os.path.abspath(__file__))
-CYLCROOT=os.path.dirname(os.path.dirname(os.path.dirname(CURR_PATH)))
-CYLCPATH=os.environ.get('CYLCPATH',CYLCROOT)
-MONITOBS=os.environ.get('MONITOBS',CYLCPATH+"/modules/monitobs")
-LIB=os.environ.get('LIB',MONITOBS+"/pylib")
-sys.path.append(LIB)
-DIC=os.environ.get('DIC',MONITOBS+"/pydic")
-sys.path.append(DIC)
-NML=os.environ.get('NML',MONITOBS+"/nml")
-sys.path.append(NML)
 #############################################################################################################################
 ###  Merged from imdaanowcast package
 #############################################################################################################################
-#from __future__ import print_function
-#import os,sys
-#CURR_PATH=os.path.dirname(os.path.abspath(__file__))
-#PKGHOME=os.environ.get('PKGHOME',os.path.dirname(CURR_PATH))
-#PKGNAME=os.path.basename(PKGHOME)
-#LIB=os.environ.get('LIB',PKGHOME+"/pylib")
-#sys.path.append(LIB)
-#DIC=os.environ.get('DIC',PKGHOME+"/pydic")
-#sys.path.append(DIC)
-#NML=os.environ.get('NML',PKGHOME+"/nml")
-#sys.path.append(NML)
+from __future__ import print_function
+import os,sys
+CURR_PATH=os.path.dirname(os.path.abspath(__file__))
+PKGHOME=os.environ.get('PKGHOME',os.path.dirname(CURR_PATH))
+PKGNAME=os.path.basename(PKGHOME)
+LIB=os.environ.get('LIB',PKGHOME+"/pylib")
+sys.path.append(LIB)
+DIC=os.environ.get('DIC',PKGHOME+"/pydic")
+sys.path.append(DIC)
+NML=os.environ.get('NML',PKGHOME+"/nml")
+sys.path.append(NML)
 PALETTE=os.environ.get('PYNGL_COLORMAPS',PKGHOME+"/palette")
 
-#diaglev=int(os.environ.get('GEN_MODE',0))
-#def errprint(*args, **kwargs):
-#    if diaglev > 0: print(*args, file=sys.stderr, **kwargs)
+diaglev=int(os.environ.get('GEN_MODE',0))
+def errprint(*args, **kwargs):
+    if diaglev > 0: print(*args, file=sys.stderr, **kwargs)
 
 import datadic
 #import subprocess
