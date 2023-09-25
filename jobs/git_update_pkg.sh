cff1702a95f1186e42f4fe731a04e35e60fcb8bf
Gibies George <gibies.george@gov.in>
2023-09-25 12:14:40 +0530

    Add files

--- jobs/git_update_pkg.sh
+++ jobs/git_update_pkg.sh
@@ -5,14 +5,16 @@ export GITROOT=${SELF%/jobs*}
 echo ${GITROOT}
 cd ${GITROOT}
 UTFList=$(git ls-files --others --exclude-standard)
-
+####################################################
+if [ ! -z "${UTFList}" ]; then
 echo "_______________________"
 echo "New untracked files are"
 for utf in ${UTFList}; do
 echo ${utf}
 done
 echo "_______________________"
-
+fi
+####################################################
 cd ${GITROOT}; 
 git pull
 git add ${UTFList} 
