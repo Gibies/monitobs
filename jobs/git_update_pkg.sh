a5e01c311b0f9f056c00dfa937e303bf9dbd3f0e
Gibies George <gibies.george@gov.in>
2023-09-25 12:09:49 +0530

    Add files

--- jobs/git_update_pkg.sh
+++ jobs/git_update_pkg.sh
@@ -16,7 +16,8 @@ echo "_______________________"
 cd ${GITROOT}; 
 git pull
 git add ${UTFList} 
-git commit -a -m "Add files ${UTFList}"
+git commit -a -m "Add files $(echo ${UTFList}| tr "
+" " ")"
 git push
 git status
 
