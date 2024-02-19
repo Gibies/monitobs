0a012aa0c8701fbd6e6adfec46db974e8eeb8078
Gibies George <gibies.george@gov.in>
2024-02-19 16:06:32 +0530

    Updated monitobs for

--- jobs/py2launch.sh
+++ jobs/py2launch.sh
@@ -15,19 +15,13 @@ module load gnu/python_eccodes/eccodes-2.13.1_utility
 module load gnu/py-ncepbufr/1.1.1
 module load gnu/pathlib/1.0.1
 module load gnu/cython/0.28
-module load gnu/iris/2.0.0  #
+module load gnu/iris/2.0.0  
 module load gnu/toolz/0.9.0
 module load gnu/lib/udunits/2.2.26
 module load gnu/cartopy/0.16.0
-#module load gnu/lib/netcdf_c_f_cxx/4.6.1_4.4.4_4.3.0
-#module load gnu/netcdf4/1.5.3-netcdf4.6.0hdf51.10.0
 module load gnu/utility/pynio/1.5.5
 module load gnu/pyngl/1.6.1
 module load gnu/pyke/1.1.1
-<<<<<<< HEAD
-=======
-#module load gnu/xarray/0.10.2
->>>>>>> 0a572c43fe3d42d96241451d4a8870f6f6583d07
 module load gnu/xarray/0.11.3
 
 module list
