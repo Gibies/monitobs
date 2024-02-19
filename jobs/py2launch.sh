cff467f67eeb09631128e80438043004d37c4e4b
Meenakshikj <meenakshikj28@gmail.com>
2024-02-19 15:41:00 +0530

    Updated monitobs for

--- jobs/py2launch.sh
+++ jobs/py2launch.sh
@@ -14,16 +14,15 @@ module load gnu/iris/2.0.0  #
 module load gnu/toolz/0.9.0
 module load gnu/lib/udunits/2.2.26
 module load gnu/cartopy/0.16.0
-module load gnu/lib/netcdf_c_f_cxx/4.6.1_4.4.4_4.3.0
-module load gnu/netcdf4/1.5.3-netcdf4.6.0hdf51.10.0
+#module load gnu/lib/netcdf_c_f_cxx/4.6.1_4.4.4_4.3.0
+#module load gnu/netcdf4/1.5.3-netcdf4.6.0hdf51.10.0
 module load gnu/utility/pynio/1.5.5
 module load gnu/pyngl/1.6.1
 module load gnu/pyke/1.1.1
-<<<<<<< HEAD
 #module load gnu/xarray/0.10.2
-=======
->>>>>>> 9d88f064ab322cf9c8dd6f038d09947fdcb491b1
 module load gnu/xarray/0.11.3
+
+module list
 
 python ${PYSCRYPT} ${ARGS}
 
