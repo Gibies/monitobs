49820b458954b5b8222bcde9f045a8dbeeb1243f
Gibies George <gibies.george@gov.in>
2024-03-13 17:04:48 +0530

    Updated monitobs for

--- pylib/obsmod.py
+++ pylib/obsmod.py
@@ -431,3 +431,12 @@ def obstore_create_file(obstore_info,diagflg=0,callsignflag=False,filedata=None)
 def create_obstore(obstore_info,diagflg=0,callsignflag=False,filedata=None):
 	obstore_info=obstore_create_file(obstore_info,diagflg=diagflg,callsignflag=callsignflag,filedata=filedata)
 	return(obstore_info)
+
+def field_extract(infile,varnames=None,dims=None,outfile=None):
+	datset=daview.ixn_extract(infile,varnames=varnames,dims=dims,outfile=outfile)
+	return(datset)
+
+def mkdir(path):
+	path=obslib.mkdir(path)
+	return(path)
+
